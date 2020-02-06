af1e1dfb5b4ebdea76e457cf532387ba723e6276
Dedi Rahmansah <derah666@gmail.com>
2020-02-06 07:44:01 +0700

    keterangan route

--- icrowdblog/routes/web.php
+++ icrowdblog/routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+/*
+* Keterangan :
+* url, controller@metode
+* atau
+* url, metode
+* dengan
+* return folder(namafile)
+* atau 
+* return nilai
+*/
 Route::get('/', function () {
     return view('welcome');
 });
