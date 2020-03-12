8e4c5acd8cca0d31b1ef244bd0e06b990bfe3e18
derah666@gmail.com <dedir2@gmail.com>
2020-03-12 08:34:07 +0700

    salah tampilan

--- LatihanAPI/resources/views/base.blade.php
+++ LatihanAPI/resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <!-- For Google -->
     <link rel="author" href="https://plus.google.com/+Scoopthemes">
@@ -24,11 +24,9 @@
     
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
     <div id="wrapper">
@@ -46,7 +44,9 @@
                 </li>
             </ul>
         </aside>
+
         @yield('content') {{-- Semua file konten kita akan ada di bagian ini --}}
+        
     </div>
     <!-- /#wrapper -->
 
