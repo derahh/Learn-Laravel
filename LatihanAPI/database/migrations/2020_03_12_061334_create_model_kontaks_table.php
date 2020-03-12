c021f16d3c7029ad4ffc6eda08df6d784f4c30e1
derah666@gmail.com <dedir2@gmail.com>
2020-03-12 13:27:04 +0700

    add model

---
+++ LatihanAPI/database/migrations/2020_03_12_061334_create_model_kontaks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelKontaksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('model_kontaks', function (Blueprint $table) {
+            $table->increments('id'); //membuat kolom id auto increments
+            $table->string('nama'); //membuat kolom nama
+            $table->string('email'); //membuat kolom email
+            $table->string('nohp'); //membuat kolom no hp
+            $table->string('alamat'); //membuat kolom alamat
+            $table->timestamps(); //membuat created_at dan updated_at sebagai fungsi dasar laravel
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('model_kontaks');
+    }
+}
